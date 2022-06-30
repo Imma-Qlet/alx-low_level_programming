c032d0a2f1f867c3dc635e8e8137466ec0460041
Qlet <malynqlet@gmail.com>
2022-06-30 15:41:43 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
+int main (void)
 {
-
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
+print f("with proper grammar. but the outcome is a piece of art,\n");
 return (0);
-
 }
