05a2395551bb385ad061f36fd48463c875ef1e44
Qlet <malynqlet@gmail.com>
2022-07-04 03:39:13 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,18 +10,10 @@
 
 int main(void)
 {
-int d;
+char low;
 
-for (d = 0; d < 100; d++)
-{
-putchar((d / 10) + 0);
-putchar((d % 10) + 0);
-if (d != 99)
-{
-putchar(,);
-putchar( );
-}
-}
+for (low = a; low <= z; low++)
+putchar(low);
 putchar(n);
 
 return (0);
