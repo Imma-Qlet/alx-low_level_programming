826ff4e0b8248f068bed0135ceebd656942c9b9a
Qlet <malynqlet@gmail.com>
2022-07-11 14:19:22 -0700

    string

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* file: main.h
+* Auth: Immaculate Nyakio
+* Desc: Header file containing prototypes for all functions
+
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */ 
