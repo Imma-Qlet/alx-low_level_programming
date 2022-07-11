300eb60f41cbfccc86e634e0f4d3a3e71a38fb66
Qlet <malynqlet@gmail.com>
2022-07-11 14:28:38 -0700

    string

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,12 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
-
- * reset_to_98 - Takes a pointer to an int and updates
-
- *               the value it points to to 98.
-
- * @n: The pointer to an int.
-
- */
+* reset_to_98 - Takes a pointer to an int and updates
+* the value of it points to 98
+* @n: The pointer to an int
+*/
 
 void reset_to_98(int *n)
-
 {
-
 *n = 98;
-
 }
